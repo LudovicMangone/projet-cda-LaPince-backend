b72323840a06c7569c7bce58476d63b374a05407
LudovicMangone <mangone.ludovic@gmail.com>
2026-06-11 09:09:49 +0200

    fix: add assertProjectOwner check in updateOperation

--- src/services/operations.service.ts
+++ src/services/operations.service.ts
@@ -93,6 +93,7 @@ export async function updateOperation(
 	userId: number,
 ) {
 	return prisma.$transaction(async (tx) => {
+		await assertProjectOwner(data.projectId, userId, tx);
 		const operation = await tx.operation.findUnique({
 			where: { id: operationId },
 			select: {
@@ -153,7 +154,7 @@ export async function deleteOperationsByPojectId(
 ) {
 	return prisma.$transaction(async (tx) => {
 		await assertProjectOwner(data.projectId, userId, tx);
-		
+
 		await tx.operation.delete({
 			where: { id: data.operationId, projectId: data.projectId },
 		});
