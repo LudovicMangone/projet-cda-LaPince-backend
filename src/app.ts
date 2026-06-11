dbbead85588a0e767478735602f55f6de4567753
LudovicMangone <mangone.ludovic@gmail.com>
2026-06-11 09:02:27 +0200

    fix: move xss sanitizer after express.json middleware

--- src/app.ts
+++ src/app.ts
@@ -20,8 +20,6 @@ import projectsRouter from "./routers/projects.router";
 export const app = express(); // CORS: allows external clients (frontend, tools, other APIs) to call the API
 app.set("trust proxy", 1); // Required for Render/reverse proxy
 app.use(cors({ origin: allowedOrigins }));
-// XSS sanitizer: protects against malicious scripts injected in user input
-app.use(xss());
 // Standard headers reforced, hidden infos and XSS attack blocker
 app.use((req, res, next) => {
 	if (req.path === "/" || req.path.startsWith("/-/")) return next();
@@ -29,6 +27,8 @@ app.use((req, res, next) => {
 });
 // Parses JSON request bodies: required to read req.body in POST / PATCH / PUT requests. Using a limit for payload attac
 app.use(express.json({ limit: envConfig.jsonLimit }));
+// XSS sanitizer: protects against malicious scripts injected in user input
+app.use(xss());
 // Rate limiter to protect the API against abuse and excessive requests (basic DDoS / brute-force protection)
 app.use(apiRateLimiter);
 
